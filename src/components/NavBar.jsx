0357c26ce77a9748b55f1743ef7a21e324552401
Constanza Guimaraez <contiguima@gmail.com>
2022-10-20 07:01:29 -0300

    Rutas y Antesala - Estructura

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,8 @@
+/*import React from "react";
+
+function NavBar()
+{
+    return();
+}
+
+export default NavBar;*/
---
+++ src/pages/AnteSala.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../stylesheets/AnteSala.css"
+function AnteSala()
+{
+    return(
+        <><div className="contenedor-logo">
+
+        </div>
+        
+        <div className="contenedor-botones">
+        <button> Soy médico</button>
+        <button> Soy paciente</button>
+            </div></>
+    )
+}
+
+export default AnteSala;
